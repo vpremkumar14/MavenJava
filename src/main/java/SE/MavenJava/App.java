579513dae2b3d5c7e2243919c8db5166beaad604
prem <vpremkumar1431@gmail.com>
2025-08-12 12:46:56 +0530

    mavenjava

---
+++ src/main/java/SE/MavenJava/App.java
@@ -0,0 +1,13 @@
+package SE.MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
